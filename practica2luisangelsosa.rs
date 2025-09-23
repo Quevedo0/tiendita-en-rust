a95a359845db6e0648ec849c82e857d8c5355a77
luis14893 <luisangelsosaaraujo65@gmail.com>
2025-09-23 10:52:29 -0700

    Add files via upload

---
+++ practica2luisangelsosa.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+        fn main() {
+        println!("Por favor, ingresa un número:");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Error al leer la línea");
+
+        let num: i8 = input.trim().parse().expect("Por favor, ingresa un número>
+
+        if num % 2 == 0 {
+        println!("El número {} es par.", num);
+        } else {
+        println!("El número {} es impar.", num);
+        }
+}
+
+
+
